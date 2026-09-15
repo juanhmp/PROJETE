d738387002d19eacec6b18d3871c92a9a870677a
Mariana <marianafdasilva5@gmail.com>
2026-09-15 10:38:15 -0300

    Ajustes bluetooth e mapa de calor

--- C#/obj/Debug/net10.0/c#.AssemblyInfo.cs
+++ C#/obj/Debug/net10.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d461c0234a36dced0c6396156a3ddad0c09849a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+636c860848fbb4f8430a8bfdc6be25fb259f7411")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
